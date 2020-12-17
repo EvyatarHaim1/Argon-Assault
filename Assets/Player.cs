6bdd6342fde863a4c1fe78d71befceb411147d09
Evyatar Haim <evyataros1@gmail.com>
2020-12-17 18:55:07 +0200

    fixing the player movement, add rotation with pitch, yaw and roll rules

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     [Tooltip("In ms^-1")] [SerializeField] float xRange = 15f;
     [Tooltip("In ms^-1")] [SerializeField] float yRange = 8f;
 
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = -5f;
+    [SerializeField] float controlPitchFactor = -29f;
+    [SerializeField] float controlRollFactor = -29f;
+
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +25,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * speed * Time.deltaTime;
         float yOffset = yThrow * speed * Time.deltaTime;
@@ -31,5 +44,18 @@ public class Player : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+            ;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
